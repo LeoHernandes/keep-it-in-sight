42085ce65f493586cc6c0b2996bc68b43652a7ed
Leo Hernandes <leohernandesvasc@gmail.com>
2023-12-28 18:46:11 -0300

    remove ortographic projection

--- src/main.cpp
+++ src/main.cpp
@@ -81,9 +81,6 @@ float g_CameraDistance = 3.5f; // Distância da câmera para a origem
 float g_TorsoPositionX = 0.0f;
 float g_TorsoPositionY = 0.0f;
 
-// Variável que controla o tipo de projeção utilizada: perspectiva ou ortográfica.
-bool g_UsePerspectiveProjection = true;
-
 // Variável que controla se o texto informativo será mostrado na tela.
 bool g_ShowInfoText = true;
 
@@ -839,18 +836,6 @@ void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mod)
         g_TorsoPositionY = 0.0f;
     }
 
-    // Se o usuário apertar a tecla P, utilizamos projeção perspectiva.
-    if (key == GLFW_KEY_P && action == GLFW_PRESS)
-    {
-        g_UsePerspectiveProjection = true;
-    }
-
-    // Se o usuário apertar a tecla O, utilizamos projeção ortográfica.
-    if (key == GLFW_KEY_O && action == GLFW_PRESS)
-    {
-        g_UsePerspectiveProjection = false;
-    }
-
     // Se o usuário apertar a tecla H, fazemos um "toggle" do texto informativo mostrado na tela.
     if (key == GLFW_KEY_H && action == GLFW_PRESS)
     {
@@ -922,58 +907,23 @@ void SetupOpenGl()
 
 void UpdateCamera(GLint view_uniform, GLint projection_uniform)
 {
-    // Computamos a posição da câmera utilizando coordenadas esféricas.  As
-    // variáveis g_CameraDistance, g_CameraPhi, e g_CameraTheta são
-    // controladas pelo mouse do usuário. Veja as funções CursorPosCallback()
-    // e ScrollCallback().
+    // Look at camera position
     float r = g_CameraDistance;
     float y = r * sin(g_CameraPhi);
     float z = r * cos(g_CameraPhi) * cos(g_CameraTheta);
     float x = r * cos(g_CameraPhi) * sin(g_CameraTheta);
 
-    // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
-    // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-    glm::vec4 camera_position_c = glm::vec4(x, y, z, 1.0f);             // Ponto "c", centro da câmera
-    glm::vec4 camera_lookat_l = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);      // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-    glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-    glm::vec4 camera_up_vector = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);     // Vetor "up" fixado para apontar para o "céu" (eito Y global)
-
-    // Computamos a matriz "View" utilizando os parâmetros da câmera para
-    // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
+    glm::vec4 camera_position_c = glm::vec4(x, y, z, 1.0f);
+    glm::vec4 camera_lookat_l = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c;
+    glm::vec4 camera_up_vector = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
     glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
 
-    // Agora computamos a matriz de Projeção.
-    glm::mat4 projection;
+    float nearplane = -0.1f;
+    float farplane = -10.0f;
+    float field_of_view = 3.141592 / 3.0f;
+    glm::mat4 projection = Matrix_Perspective(field_of_view, g_ScreenRatio, nearplane, farplane);
 
-    // Note que, no sistema de coordenadas da câmera, os planos near e far
-    // estão no sentido negativo! Veja slides 176-204 do documento Aula_09_Projecoes.pdf.
-    float nearplane = -0.1f; // Posição do "near plane"
-    float farplane = -10.0f; // Posição do "far plane"
-
-    if (g_UsePerspectiveProjection)
-    {
-        // Projeção Perspectiva.
-        // Para definição do field of view (FOV), veja slides 205-215 do documento Aula_09_Projecoes.pdf.
-        float field_of_view = 3.141592 / 3.0f;
-        projection = Matrix_Perspective(field_of_view, g_ScreenRatio, nearplane, farplane);
-    }
-    else
-    {
-        // Projeção Ortográfica.
-        // Para definição dos valores l, r, b, t ("left", "right", "bottom", "top"),
-        // PARA PROJEÇÃO ORTOGRÁFICA veja slides 219-224 do documento Aula_09_Projecoes.pdf.
-        // Para simular um "zoom" ortográfico, computamos o valor de "t"
-        // utilizando a variável g_CameraDistance.
-        float t = 1.5f * g_CameraDistance / 2.5f;
-        float b = -t;
-        float r = t * g_ScreenRatio;
-        float l = -r;
-        projection = Matrix_Orthographic(l, r, b, t, nearplane, farplane);
-    }
-
-    // Enviamos as matrizes "view" e "projection" para a placa de vídeo
-    // (GPU). Veja o arquivo "shader_vertex.glsl", onde estas são
-    // efetivamente aplicadas em todos os pontos.
     glUniformMatrix4fv(view_uniform, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, glm::value_ptr(projection));
 }
